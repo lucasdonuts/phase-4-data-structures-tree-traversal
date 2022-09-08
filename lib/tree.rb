cb9bb3d0064e1c3f85c58ba298ba67e76d2d0f6b
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-08 10:26:02 -0500

    get_element_by_id implemented with breadth- and depth-first traversal

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,32 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  # depth-first
+  # def get_element_by_id(id)
+  #   match = nil
+  #   to_visit = [@root]
+  #   until to_visit.empty?
+  #     node = to_visit[0]
+  #     match = node if node[:id] == id
+  #     to_visit.shift
+  #     to_visit = node[:children] + to_visit
+  #   end
+  #   match
+  # end
+
+  # breadth-first
+  def get_element_by_id(id)
+    match = nil
+    to_visit = [@root]
+    until to_visit.empty?
+      node = to_visit[0]
+      match = node if node[:id] == id
+      to_visit.shift
+      node[:children].each do |c|
+        to_visit.push(c)
+      end
+    end
+    match
+  end
 end
